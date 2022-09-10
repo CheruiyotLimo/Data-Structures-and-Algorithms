3e9388cc54e3a0115fe8b672d1af7142f6cfc437
Yoni Limo <limzyon@gmail.com>
2022-09-10 20:38:34 +0300

    Still unfinished on Lomuto partition.

--- quick_sort.py
+++ quick_sort.py
@@ -23,15 +23,35 @@ def hoare_partition(start, end, elements):
     swapper(pivot_index, end, elements)
     return end
 
+def lomuto_partition(elements):
+    pivot_index = len(elements)-1
+    pivot = elements[pivot_index]
+     
+    p_index = 0
+    i = 0
+    while p_index < len(elements)-1:
+        while elements[p_index] <= pivot:
+            p_index += 1
+        i = p_index
+        if i == len(elements)-1:
+            break
+        while elements[i] > pivot:
+            i += 1
+        swapper(p_index, i, elements)
+    return p_index
 
-def quick_sort(start, end, elements):
-    if start < end:
-        pi = hoare_partition(start, end, elements)
-        quick_sort(0, pi - 1, elements)
-        quick_sort(pi + 1, end, elements)
 
+# def quick_sort(start, end, elements):
+#     if start < end:
+#         pi = hoare_partition(start, end, elements)
+#         quick_sort(0, pi - 1, elements)
+#         quick_sort(pi + 1, end, elements)
+
+def quick_sort(elements):
+    lomuto_partition(elements)
 
 if __name__ == "__main__":
     lst = [12, 4, 56, 7, 23, 13, 20]
-    quick_sort(0, len(lst)-1, lst)
+    # quick_sort(0, len(lst)-1, lst)
+    quick_sort(lst)
     print(lst)
