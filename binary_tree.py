438129945962dcd8f0fdf2e131dfa198e593b0d9
Yoni Limo <limzyon@gmail.com>
2022-11-15 01:00:24 +0300

    BST

--- binary_tree.py
+++ binary_tree.py
@@ -86,85 +86,19 @@ class Queue2():
             return "The queue is empty"
         return self.arr.head.prev.data
 
-class BinaryTreeNode():
+class TreeNode():
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
-    def add_child(self, value):
-        if self.data == value:
-            return
-        if value < self.data:
-            if self.left:
-                return self.left.add_child(value)
-            else:
-                self.left = BinaryTreeNode(value)
+    
+    def add_child(self, data):
+        if not self:
+            self = TreeNode(data)
         else:
-            if self.right:
-                return self.right.add_child(value)
-            else:
-                self.right = BinaryTreeNode(value)
-    
-    def search(self, value):
-        if self.data == value:
-            return True
-        if value < self.data:
-            if self.left:
-                return self.left.search(value)
-            else:
-                return False
-        
-        if value > self.data:
-            if self.right:
-                return self.right.search(value)
-            else:
-                return False
 
-    def in_order_traversal(self):
-        elements = []
-        if self.left:
-            elements += self.left.in_order_traversal()
-        elements.append(self.data)
-        if self.right:
-            elements += self.right.in_order_traversal()
-        # print(elements)
-        return elements
 
-    def find_min(self):
-        if self.left is None:
-            return self.data
-        return self.left.find_min()
-    def find_max(self):
-        if self.right is None:
-            return self.data
-        return self.right.find_max()
-    def calculate_sum(self):
-        count = 0
-        for el in self.in_order_traversal():
-            count += el
-        return count
-
-    def pre_order_traversal(self):
-        elements = []
-        elements.append(self.data)
-        if self.left:
-            elements += self.left.pre_order_traversal()
-        if self.right:
-            elements += self.right.pre_order_traversal()
-        return elements
-    
-    def post_order_traversal(self):
-        elements = []
-        if self.left:
-            elements += self.left.post_order_traversal()
-        if self.right:
-            elements += self.right.post_order_traversal()
-        elements.append(self.data)
-        return elements
-
-    def level_order_traversal(self):
-        elements = []
+    def search(self, data):   #Preferable to use level-order traversal.
         if not self:
             return
         else:
@@ -172,48 +106,23 @@ class BinaryTreeNode():
             qu.enqueue(self)
             while not qu.isEmpty():
                 node = qu.dequeue()
-                elements.append(node.data)
+                if data == node:
+                    return "Found the data."
                 if node.left:
                     qu.enqueue(node.left)
                 if node.right:
                     qu.enqueue(node.right)
-            return elements
+            return "Data not found."
 
-    def delete(self, value):
-        if value < self.data:
-            if self.left:
-                self.left = self.left.delete(value = value)
-        if value > self.data:
-            if self.right:
-                self.right = self.right.delete(value = value)
-        else:
-            if self.left is None and self.right is None:
-                return
-            if self.right is None:
-                return self.left
-            if self.left is None:
-                return self.right
-            else:
-                min_val = self.left.find_max()
-                self.data = min_val
-                self.left = self.left.delete(min_val)
-        return self
-
-def tree_builder(numbers):
-    root = BinaryTreeNode(numbers[0])
-    for i in range(1, len(numbers)):
-        root.add_child(numbers[i])
-    return root
+    
 
 
-x = tree_builder([17, 4, 1, 20, 9, 23, 18, 34])
-print(x)
-print(["In order: "],  x.in_order_traversal())
-# print(x.find_min())
-# print(x.find_max())
-# print(x.calculate_sum())
-print(["Pre_order: "], x.pre_order_traversal())
-print(["Post-order: "], x.post_order_traversal())
-# print(x.delete(4))
-print(["In order: "],  x.in_order_traversal())
-print("Level Order: ",  x.level_order_traversal())
+
+el = TreeNode("Electronics")
+phone = TreeNode("Phones")
+books = TreeNode("Books")
+el.add_child(books)
+el.add_child(phone)
+phone.add_child(TreeNode("iPhone"))
+phone.add_child(TreeNode("Samsung"))
+phone.add_child(TreeNode("Pixel"))
