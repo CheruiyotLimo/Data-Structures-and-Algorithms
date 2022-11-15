0de7969bcd70a3d989a1811a3d9da1519f9a68c4
Yoni Limo <limzyon@gmail.com>
2022-11-15 11:36:25 +0300

    Updated traversal methods

--- binary_tree.py
+++ binary_tree.py
@@ -92,11 +92,63 @@ class TreeNode():
         self.left = None
         self.right = None
     
-    def add_child(self, data):
+    def print_tree(self):
+        # spaces = " " * self.get_level() * 3
+        print(self.data)
+        if self.left:
+            self.left.print_tree()
+        if self.right:
+            self.right.print_tree()
+        return
+    
+    def pre_order_traversal(self):          #Depth-first traversal
         if not self:
-            self = TreeNode(data)
+            return
         else:
+            print(self.data)
+            self.pre_order_traversal(self.left)
+            self.pre_order_traversal(self.right)
+    
+    def in_order_traversal(self):           ##Depth-first traversal
+        if not self:
+            return
+        else:
+            self.pre_order_traversal(self.left)
+            print(self.data)
+            self.pre_order_traversal(self.right)
+    
+    def post_order_traversal(self):         #Depth-first traversal
+        if not self:
+            return
+        else:
+            self.pre_order_traversal(self.left)
+            self.pre_order_traversal(self.right)
+            print(self.data)
 
+    def level_order_traversal(self):      #Breadth first traversal
+        if not self:
+            return
+        else:
+            qu = Queue2()
+            qu.enqueue(self)
+            while not qu.isEmpty():
+                node = qu.dequeue()
+                if not node:
+                    print(node.data)
+                    qu.enqueue(self.left)
+                    qu.enqueue(self.right)
+
+
+    def add_child(self, data):
+        node = TreeNode(data)
+        if not self:
+            self = node
+        else:
+            if self.left:
+                self.right = node
+            else:
+                self.left = node
+            return
 
     def search(self, data):   #Preferable to use level-order traversal.
         if not self:
@@ -125,4 +177,5 @@ el.add_child(books)
 el.add_child(phone)
 phone.add_child(TreeNode("iPhone"))
 phone.add_child(TreeNode("Samsung"))
-phone.add_child(TreeNode("Pixel"))
+# phone.add_child(TreeNode("Pixel"))
+el.print_tree()
