743b2dcfe091fdc75c434d0a0affe986b4c471c4
Yoni Limo <limzyon@gmail.com>
2023-01-10 14:32:58 +0300

    kruskal algorithm

--- Graphs/kruskal_algorithm.py
+++ Graphs/kruskal_algorithm.py
@@ -1,8 +1,8 @@
 import disjoint_set as dst
 
 class Graph:
-    def __init__(self):
-        self.vertices = []
+    def __init__(self, vertices):
+        self.vertices = vertices
         self.graph = []
         self.nodes = []
         self.mst = []
@@ -31,3 +31,28 @@ class Graph:
                 self.mst.append([s, d, w])
                 ds.union(x, y)
         self.print_solution(s, d, w)
+
+
+g = Graph(5)
+g.add_node("A")
+g.add_node("B")
+g.add_node("C")
+g.add_node("D")
+g.add_node("E")
+
+g.add_edge("A", "B", 5)
+g.add_edge("A", "C", 13)
+g.add_edge("A", "E", 15)
+g.add_edge("B", "A", 5)
+g.add_edge("B", "C", 10)
+g.add_edge("B", "D", 8)
+g.add_edge("C", "A", 13)
+g.add_edge("C", "B", 10)
+g.add_edge("C", "E", 20)
+g.add_edge("C", "D", 6)
+g.add_edge("D", "B", 8)
+g.add_edge("D", "C", 6)
+g.add_edge("E", "A", 15)
+g.add_edge("E", "C", 20)
+
+g.kruskal_algorithm()
