6a45b3351952b25c40197750b011addb2385c020
Yoni Limo <limzyon@gmail.com>
2023-01-12 15:20:55 +0300

    Added a cycle detector

--- Graphs/topological_sort.py
+++ Graphs/topological_sort.py
@@ -24,12 +24,38 @@ class Graph:
                 self.topo_sort_util(j, visited, stack)
         print(stack)
 
+    def cycle_checker(self):
+        visited = []
+        stack = []
+        for key in self.graph:
+            if key not in visited:
+                if self.dfs(key, list(), visited, stack):
+                    return True
+        return False
+
+    def dfs(self, vert, path, visited, stack):
+        path.append(vert)
+        visited.append(vert)
+        for i in self.graph[vert]:
+            if i == vert:
+                continue
+            if i in path:
+                return True
+            if i not in visited and self.dfs(i, path, visited, stack):
+                return True
+        path.pop()
+        return False
+
 
 # if __name__ == "__main__":
 custom_gr = Graph(6)
 custom_gr.add_edge("A", "C")
 custom_gr.add_edge("B", "D")
 custom_gr.add_edge("C", "E")
+# custom_gr.add_edge("C", "A")
 custom_gr.add_edge("E", "F")
+# custom_gr.add_edge("E", "A")
 custom_gr.add_edge("A", "G")
 custom_gr.topological_sort()
+print(custom_gr.graph)
+print(custom_gr.cycle_checker())
