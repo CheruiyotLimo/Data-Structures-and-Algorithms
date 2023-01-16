8a08f4c0e9c2bc193ac6f87d088b6b7a8072a956
Yoni Limo <limzyon@gmail.com>
2023-01-16 23:59:07 +0300

    __

--- binary_heap.py
+++ binary_heap.py
@@ -1,24 +1,26 @@
-
 class BinaryHeap:
     def __init__(self, size) -> None:
         self.max_size = size + 1
-        self.cust_list = [None] * (size+1)
+        self.cust_list = [None] * (size + 1)
         self.heap_size = 0
+
 
 def peek(root):
     if not root:
         return
     return root.cust_list[1]
 
+
 def size_of_heap(root):
     if not root:
         return
     return root.heap_size
 
+
 def heapify(root, index, type):
     if index <= 1:
         return
-    parent_index = int(index/2)
+    parent_index = int(index / 2)
     if type == "Min":
         if root.cust_list[index] < root.cust_list[parent_index]:
             temp = root.cust_list[parent_index]
@@ -31,20 +33,23 @@ def heapify(root, index, type):
             root.cust_list[parent_index] = root.cust_list[index]
             root.cust_list[index] = temp
         heapify(root, parent_index, "Max")
-    
+
+
 def level_order_traversal(root):
     if not root:
         return
-    for i in range(1, root.heap_size+1):
+    for i in range(1, root.heap_size + 1):
         print(root.cust_list[i])
+
 
 def insert_node(root, node, heap_type):
     if root.heap_size + 1 == root.max_size:
         return "The Binary Heap is full."
-    root.cust_list[root.heap_size+1] = node
+    root.cust_list[root.heap_size + 1] = node
     root.heap_size += 1
     heapify(root, root.heap_size, heap_type)
     return "Done"
+
 
 def heapify_extract(root, index, type):
     left_index = index * 2
@@ -65,7 +70,7 @@ def heapify_extract(root, index, type):
                 root.cust_list[left_index] = root.cust_list[index]
                 root.cust_list[index] = temp
             return
-    else: 
+    else:
         if type == "Min":
             if root.cust_list[left_index] > root.cust_list[right_index]:
                 swap_child = right_index
@@ -74,21 +79,22 @@ def heapify_extract(root, index, type):
             if root.cust_list[index] > root.cust_list[swap_child]:
                 temp = root.cust_list[swap_child]
                 root.cust_list[swap_child] = root.cust_list[index]
-                root.cust_list[index] = temp   
-        else:  
+                root.cust_list[index] = temp
+        else:
             if root.cust_list[left_index] > root.cust_list[right_index]:
                 swap_child = right_index
             else:
-                swap_child = left_index       
+                swap_child = left_index
             if root.cust_list[index] < root.cust_list[swap_child]:
                 temp = root.cust_list[swap_child]
                 root.cust_list[swap_child] = root.cust_list[index]
                 root.cust_list[index] = temp
         heapify_extract(root, swap_child, type)
-    
+
+
 def extract_node(root, type):
     if root.heap_size == 0:
-        return 
+        return
     else:
         extract = root.cust_list[1]
         root.cust_list[1] = root.cust_list[root.heap_size]
@@ -96,6 +102,7 @@ def extract_node(root, type):
         root.heap_size -= 1
         heapify_extract(root, 1, type)
         return extract
+
 
 bh = BinaryHeap(6)
 insert_node(bh, 10, "Min")
--- rough.py
+++ rough.py
@@ -1,3 +1,23 @@
-d = {"A": [1, 3, 4,5],
-     "B": [(9, 8, 0)]}
-print(d.get("A", ()))
+import heapq
+
+class KthLargest:
+    def __init__(self, k, nums):
+        self.k = k
+        # if nums:
+        heapq.heapify(nums)
+        self.nums = nums
+
+list1 = [5, 7, 9, 1, 3]
+kl = KthLargest(3, list1)
+print(heapq.heappop(kl.nums))
+print(heapq.heappop(kl.nums))
+# # heapq.heapify(list1)
+# print(list1)
+# # print(heapq.heappush(list1, 10))
+# # print(heapq.heappush(list1, 4))
+# print(list1)
+# heapq.heappop(list1)
+# heapq.heappop(list1)
+# heapq.heappop(list1)
+# print(list1)
+print(kl.nums)
