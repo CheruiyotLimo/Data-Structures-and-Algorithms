29e40f6b94573180534b01031aba22be3299fe94
Yoni Limo <limzyon@gmail.com>
2023-01-26 22:10:01 +0300

    __

--- rough.py
+++ rough.py
@@ -1,55 +1,9 @@
-import heapq
-class MedianFinder:
+from collections import defaultdict, deque
+x = defaultdict(list)
+x["a"] = [1, 3, 4]
+x["b"] = [1, 3, 4]
+x["c"] = [1, 3, 5]
 
-    def __init__(self):
-        self.heap = []
+del x["a"]
 
-    def addNum(self, num: int) -> None:
-        self.heap.append(num)
-        heapq.heapify(self.heap)
-
-    def findMedian(self) -> float:
-        m_index = len(self.heap)//2
-        repl = []
-        if len(self.heap)%2 == 0:      # Even number
-            for _ in range(m_index):
-                y = heapq.heappop(self.heap)
-                repl.append(y)
-            z = heapq.heappop(self.heap)
-            repl.append(z)
-            for i in repl:
-                heapq.heappush(self.heap, i)
-            return (y+z)/2
-        else:       # Odd number
-            for _ in range(m_index+1):
-                x = heapq.heappop(self.heap)
-                repl.append(x)
-            for i in repl:
-                heapq.heappush(self.heap, i)
-            return x
-
-# Trying to use a sorted list.
-class MedianFinder2:
-
-    def __init__(self):
-        self.heap = []
-
-    def addNum(self, num: int) -> None:
-        self.heap.append(num)
-        qs = sorted(self.heap)
-
-    def findMedian(self) -> float:
-        qs = sorted(self.heap)
-        m_index = len(self.heap)//2
-        if len(qs) % 2 == 0:
-            return (qs[m_index]+qs[m_index-1])/2
-        else:
-            return qs[m_index]
-
-mf = MedianFinder()
-mf.addNum(1)
-mf.addNum(2)
-print(mf.findMedian())
-mf.addNum(3)
-print(mf.heap)
-print(mf.findMedian())
+print(x)
