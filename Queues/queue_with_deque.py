1afb104521ecd0543713843d00d91b14cd17e95a
Yoni Limo <limzyon@gmail.com>
2023-01-30 15:22:23 +0300

    queue using deque

---
+++ Queues/queue_with_deque.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Queue():
+    def __init__(self) -> None:
+        self.arr = deque()
+    def enqueue(self, data):
+        self.arr.appendleft(data)
+
+    def dequeue(self):
+        return self.arr.pop()
+
+    def peek(self):
+        self.arr[-1]
+
+    def length(self):
+        return len(self.arr)
+
+    def is_empty(self):
+        return len(self.arr) == 0
