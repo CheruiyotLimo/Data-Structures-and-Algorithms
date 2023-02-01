ad40b94ae3fb1b87e6f0bafa18ed6b84670401f0
Yoni Limo <limzyon@gmail.com>
2023-02-01 14:20:04 +0300

    __

--- rough.py
+++ rough.py
@@ -1,15 +1,11 @@
 from collections import defaultdict, deque
 import heapq
-x = defaultdict(list)
-x["a"] = [100]
-x["b"] = [250]
-x["c"] = [135]
+x = [
+    ["A", 3, 2],
+    ["B", 4, 1]
+]
 
-y = []
 
-for i in x:
-    y.append(x[i][0])
+x.sort(key=lambda x: x[1])
 
-heapq.heapify(y)
-# print(heapq.heappop(y))
-print(y)
+print(x)
