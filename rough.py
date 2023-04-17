86debcf349538ea4df00d90f130388e56d377b7d
Yoni Limo <limzyon@gmail.com>
2023-04-17 12:49:27 +0300

    Updated cycle_detector

--- rough.py
+++ rough.py
@@ -1,13 +1,3 @@
-def fibonacci(n):
-    if n == 0:
-        return ValueError
-    if n == 1:
-        return 0
-    if n == 2:
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
+list1 = []
 
-
-for i in range(20):
-    print(fibonacci(i))
+print(len(list1))
